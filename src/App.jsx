97470537adff8bd6380e899241f3e9d77be4bcf4
danjoebailey <danjoebailey@gmail.com>
2026-05-07 11:38:12 -0400

    rankings: also hide history from Obi genre filter (subject-driven, not craft)

--- src/App.jsx
+++ src/App.jsx
@@ -5630,7 +5630,7 @@ function RankingsTab({ books, onSaveScores, userId, authorTiers = {}, seriesTier
     return ["All", ...genres];
   }, [readBooks]);
 
-  const AI_GENRE_EXCLUDED = new Set(["Other", "Self-Help", "Young Adult", "Graphic Novel", "Biography"]);
+  const AI_GENRE_EXCLUDED = new Set(["Other", "Self-Help", "Young Adult", "Graphic Novel", "Biography", "History"]);
   const availableGenres = mode === "ai" ? ["All", ...GENRES.filter(g => !AI_GENRE_EXCLUDED.has(g))] : userGenres;
 
   // Author groupings from read books
